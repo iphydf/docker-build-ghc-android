ed0297e48fc7a33607b6b7e0a4088db0b1f7d2fc
iphydf <iphydf@users.noreply.github.com>
2016-08-25 20:47:32 +0100

    Fix cabal script

--- user-scripts/build-cross-compile-cabal.sh
+++ user-scripts/build-cross-compile-cabal.sh
@@ -9,7 +9,7 @@ cabal update
 cabal install Cabal cabal-install --bindir=$GHC_STAGE0_PREFIX/bin/
 
 # Write cabal wrapper
-echo '#/!bin/bash'                            >  "$GHC_PREFIX/bin/$NDK_TARGET-cabal"
+echo '#!/bin/bash'                            >  "$GHC_PREFIX/bin/$NDK_TARGET-cabal"
 echo "NDK=$GHC_PREFIX"                        >> "$GHC_PREFIX/bin/$NDK_TARGET-cabal"
 echo "NDK_TARGET=$NDK_TARGET"                 >> "$GHC_PREFIX/bin/$NDK_TARGET-cabal"
 echo "NEW_CABAL=$GHC_STAGE0_PREFIX/bin/cabal" >> "$GHC_PREFIX/bin/$NDK_TARGET-cabal"
